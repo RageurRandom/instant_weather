1814b548b4613f0016b928e9311c8fdfc2108226
Leo Berranger <lele.berranger@gmail.com>
2024-10-02 16:35:21 +0200

    test concluant pour une requete

--- script/api_meteo.js
+++ script/api_meteo.js
@@ -1,11 +1,15 @@
 const TOKEN = '426a99a5e0024d3e16f3622e499809dc4d55ff6f651cf0f00a67a0353d18bd88'; //500 appels/jour maximum
 
-let request = '/api/location/beaches';
+let request = '/api/location/city';
 
-fetch('https://api.meteo-concept.com'+request+'?token='+TOKEN).then(response => {
+//fetch('https://api.meteo-concept.com'+request+'?token='+TOKEN)
+fetch("https://api.meteo-concept.com/api/ephemeride/0?token=426a99a5e0024d3e16f3622e499809dc4d55ff6f651cf0f00a67a0353d18bd88") //TEST
+.then(response => {
     if(!response.ok) throw new Error('Problème de réponse');
 
-    return response.json;
+    return response.json();
 }).then(data => {
     console.log(data);
+}).catch(error => {
+    console.error('Problème de fetch', error);
 });
