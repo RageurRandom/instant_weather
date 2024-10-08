980667aa34dd2d92540d707954534834a1677cee
fortin233 <lukas.fortin@etu.unicaen.fr>
2024-10-08 09:27:37 +0200

    récupération des infos utiles pour la V1

--- script/api_meteo.js
+++ script/api_meteo.js
@@ -1,22 +1,31 @@
 const TOKEN = '426a99a5e0024d3e16f3622e499809dc4d55ff6f651cf0f00a67a0353d18bd88'; //500 appels/jour maximum
 
-let request = '/api/location/city';
 
-async function getResponse(request){
+//let request = '/api/location/city';
+
+async function getResponse(insee){
     let jsonDoc;
-
-    await fetch("https://api.meteo-concept.com"+request+"?token="+TOKEN) //TEST
-    .then(response => {
-        if(!response.ok) throw new Error('Problème de réponse');
-
-        jsonDoc = response.json();
-        console.log(jsonDoc + "\n\n" + response.json());
-    }).catch(error => {
+    try {
+    const response = await fetch("https://api.meteo-concept.com/api/forecast/daily/0?token="+TOKEN+"&insee="+insee) //TEST
+    if(!response.ok) throw new Error('Problème de réponse:' + response.status);
+    jsonDoc = await response.json();
+        //console.log(jsonDoc);
+    }
+    catch(error) {
         console.error('Problème : ', error);
-    });
+    };
 
     return jsonDoc;
 }
 
-
-console.log(getResponse("/api/ephemeride/0")+"\nDONE");
+getResponse(35238).then(data => {
+    const forecast = data.forecast; // Accéder à l'objet "forecast"
+    if (forecast) {
+        console.log(forecast.tmin + "\nDONE");
+        console.log(forecast.tmax + "\nDONE");
+        console.log(forecast.probarain + "\nDONE");
+        console.log(forecast.sun_hours + "\nDONE");
+    } else {
+        console.log("Prévisions non disponibles");
+    }
+});
