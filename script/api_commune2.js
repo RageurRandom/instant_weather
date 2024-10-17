880c8176c87744245ac7fc2c0f9392f44a74fa32
Leo Berranger <lele.berranger@gmail.com>
2024-10-17 15:53:16 +0200

    ajout d'une fonction pour faire une carte via JS

--- script/api_commune2.js
+++ script/api_commune2.js
@@ -13,10 +13,10 @@ const affichage = document.getElementById('affichage');
 const cityElement = document.getElementById('city');
 const dateElement = document.getElementById('date');
 const avgTempElement = document.getElementById('avg-temp');
-const maxTempElement = document.getElementById('max-temp');
-const minTempElement = document.getElementById('min-temp');
-const windElement = document.getElementById('wind');
-const humidityElement = document.getElementById('humidity');
+//const maxTempElement = document.getElementById('max-temp');
+//const minTempElement = document.getElementById('min-temp');
+//const windElement = document.getElementById('wind');
+//const humidityElement = document.getElementById('humidity');
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -45,11 +45,70 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function addOption(value, text) {
-    const option = document.createElement('option'); 
-    option.value = value;                            
-    option.text = text;                              
-    dropDown.appendChild(option);                    
+    const option = document.createElement('option');
+    option.value = value;
+    option.text = text;
+    dropDown.appendChild(option);
 }
+
+function makeMeteoCard(data, date) {
+    const forecast = data.forecast;
+
+    validateButton.insertAdjacentHTML("afterend" ,`<div class="flex items-center justify-center p-4">
+    <div class="flex flex-col bg-white rounded p-4 w-full max-w-xs">
+        <div class="font-bold text-xl" >${data.city.name}</div>
+        <div class="text-sm text-gray-500" id="date">${date}</div>
+        <div
+        class="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24"
+        >
+        <svg
+            class="w-32 h-32"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <img src="img/lightning.png" alt="clouds">
+
+        </svg>
+        </div>
+        <div class="flex flex-row items-center justify-center mt-6">
+        <div class="font-medium text-6xl">${Math.round((forecast.tmin + forecast.tmax) / 2)}°</div>
+        <div class="flex flex-col items-center ml-6">
+            <div>Cloudy</div>
+            <div class="mt-1">
+            <span class="text-sm"
+                ><i class="far fa-long-arrow-up"></i
+            ></span>
+            <span class="text-sm font-light text-gray-500"
+                >${forecast.tmax}°C</span
+            >
+            </div>
+            <div>
+            <span class="text-sm"
+                ><i class="far fa-long-arrow-down"></i
+            ></span>
+            <span class="text-sm font-light text-gray-500"
+                >${forecast.tmin}°C</span
+            >
+            </div>
+        </div>
+        </div>
+        <div class="flex flex-row justify-between mt-6">
+        <div class="flex flex-col items-center">
+            <div class="font-medium text-sm">Vent</div>
+            <div class="text-sm text-gray-500">${forecast.wind10m} km/h</div>
+        </div>
+        <div class="flex flex-col items-center">
+            <div class="font-medium text-sm">Probabilité de pluie</div>
+            <div class="text-sm text-gray-500">${forecast.probarain} %</div>
+        </div>
+        </div>
+    </div>
+    </div>`);
+}
+
+
 
 dropDown.addEventListener('change', function () {
     selectedCity = dropDown.value;
@@ -60,7 +119,7 @@ async function fetchByPostalCode(postalCode) {
     try {
         const response = await fetch(`https://geo.api.gouv.fr/communes?codePostal=${postalCode}`);
         const data = await response.json();
-        
+
         dropDown.innerHTML = '';
 
         if (data.length === 1) {
@@ -91,34 +150,44 @@ postalCodeInput.addEventListener('keypress', function (e) {
     }
 });
 
-async function getResponse(insee){
+async function getResponse(insee) {
     let jsonDoc;
     try {
-    const response = await fetch("https://api.meteo-concept.com/api/forecast/daily/"+dayRange+"?token="+TOKEN+"&insee="+insee) //TEST TODO
-    console.log(response);
-    if(!response.ok) throw new Error('Problème de réponse:' + response.status);
-    jsonDoc = await response.json();
+        const response = await fetch("https://api.meteo-concept.com/api/forecast/daily/" + dayRange + "?token=" + TOKEN + "&insee=" + insee) //TEST TODO
+        console.log(response);
+        if (!response.ok) throw new Error('Problème de réponse:' + response.status);
+        jsonDoc = await response.json();
         //console.log(jsonDoc);
     }
-    catch(error) {
+    catch (error) {
         console.error('Problème : ', error);
     };
 
     return jsonDoc;
 }
 
-validateButton.addEventListener('click', function () { //TODO
-    getResponse(dropDown.value).then(data => {//TODO
-        const forecast = data.forecast; //TODO
+validateButton.addEventListener('click', function () {
+    getResponse(dropDown.value).then(data => {
+        const forecast = data.forecast;
         if (forecast) {
-    
-            minTempElement.textContent = forecast.tmin + '°';
-            maxTempElement.textContent = forecast.tmax + '°';
-            avgTempElement.textContent = Math.round((forecast.tmin + forecast.tmax) / 2) + '°';
-            cityElement.textContent = data.city.name;
-            windElement.textContent = forecast.wind10m + ' k/h';
-            humidityElement.textContent = forecast.probarain + ' %';
-            console.log(forecast);
+            makeMeteoCard(data, new Date(Date.now()).toLocaleDateString('fr-FR', {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                timeZone: 'UTC'
+            }));
+            console.log("TEST");
+
+            // minTempElement.textContent = forecast.tmin + '°';
+            // maxTempElement.textContent = forecast.tmax + '°';
+            // avgTempElement.textContent = Math.round((forecast.tmin + forecast.tmax) / 2) + '°';
+            // cityElement.textContent = data.city.name;
+            // windElement.textContent = forecast.wind10m + ' k/h';
+            // humidityElement.textContent = forecast.probarain + ' %';
+            // console.log(data);
         }
-    })
+    }).catch(
+        (data) => console.log("problème : " + data)
+    );
 })
